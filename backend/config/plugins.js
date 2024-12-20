9efa3715f36058814f4a28a4349d950244802ae8
bouzid <elmokhtarie1096@gmail.com>
2024-12-20 17:54:29 +0100

    add reservation controller, make fields required

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = {
+    documentation: {
+        enabled: true,
+        config: {
+            info: {
+                version: '1.0.0',
+                description: 'API documentation for my Strapi project',
+            },
+        },
+    },
+};  
