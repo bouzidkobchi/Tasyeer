653e2902e27457994b792e548da15e494d646c3a
Abderahmane Ouggad <abderahmaneouggad@gmail.com>
2024-12-20 11:20:03 +0100

    SideBar add Fixed

--- frontend/src/myComponents/Sidebar/SideBar.tsx
+++ frontend/src/myComponents/Sidebar/SideBar.tsx
@@ -5,11 +5,13 @@ import SideBarLinks from "./SideBarLinks";
 
 export default function SideBar(){
 
-    return <div className="p-6  min-w-[300px] max-w-[400px] h-[100vh] bg-slate-100">
-        <Header />
+    return <div className="p-6  min-w-[300px] max-w-[400px] min-h-[100vh] bg-slate-100">
+        <div className="fixed">
+            <Header />
         {/* separator */}
         <div className="bg-gray-400 h-[1px] w-full my-6" />
         {/* separator */}
         <SideBarLinks />
+        </div>
     </div>
 }
